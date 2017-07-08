a8df10e087d6cba0013009ad7186184692cd3cba
Tony <lin1000@gmail.com>
2017-07-08 14:50:27 +0800

    add websocket

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # tonylin.profile.aws
 Tony Lin's Profile build on python and flask on aws
 
+
+
 Rquired Use Case
 - [] A landing page using bootstrap
 - [] Embed linkedin widet on the landing page
@@ -9,3 +11,8 @@ Rquired Use Case
 
 Optional Featuresa
 - [] Support for Google Search Console 
+- [] SocketIO listener to respond to realtime api traffic visualization
+
+Python Version 
+
+[python 2.7]
--- application.py
+++ application.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_socketio import SocketIO
 
 # print a nice greeting.
 def say_hello(username = "World"):
@@ -6,16 +7,17 @@ def say_hello(username = "World"):
 
 # some bits of text for the page.
 header_text = '''
-    <html>\n<head> <title>EB Flask Test</title> </head>\n<body>'''
+    <html>\n<head> <title>EB Flask Test with websocket </title> </head>\n<body>'''
 instructions = '''
-    <p><em>Hint</em>: This is a RESTful web service! Append a username
-    to the URL (for example: <code>/Thelonious</code>) to say hello to
-    someone specific.</p>\n'''
+    There is an endpoint of websocket 
+    \n'''
 home_link = '<p><a href="/">Back</a></p>\n'
 footer_text = '</body>\n</html>'
 
 # EB looks for an 'application' callable by default.
 application = Flask(__name__)
+application.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(application)
 
 # add a rule for the index page.
 application.add_url_rule('/', 'index', (lambda: header_text +
@@ -26,9 +28,24 @@ application.add_url_rule('/', 'index', (lambda: header_text +
 application.add_url_rule('/<username>', 'hello', (lambda username:
     header_text + say_hello(username) + home_link + footer_text))
 
+@socketio.on('message')
+def handle_message(message):
+    print 'received message: {%s}' % (message)
+
+@socketio.on('my event')
+def handle_my_custom_event(json):
+    print 'received json: {%s}' % (str(json))
+
+@socketio.on('2017_api_traffic_demo')
+def handle_2017_api_traffic_demo(json):
+    #Inbox of API Traffic and stored into a temp storage space or streaming container
+    #Then visualize the traffic on a Ajax HTML page to demonstrate it.
+    print 'received json: {%s}' % (str(json))
+
 # run the app.
 if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
     # removed before deploying a production app.
     application.debug = True
-    application.run()
+    #application.run()
+    socketio.run(application)
